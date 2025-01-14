fa0f62934afca5b4f70be0aa0152843823b6fec3
MikeNelli <themikenel@gmail.com>
2025-01-14 21:28:45 +0300

    Update Learning.py

--- Learning.py
+++ Learning.py
@@ -1,66 +1,87 @@
-# def decor(input_func):
-#     def result(*args):
-#         print("\n>>> Called decorated func <<<", end="\n")
-#         out = input_func(*args)
-#         print("\n\t^^^")
-#         return not out
-#     return result
+def decor(input_func):
+    def result(*args):
+        print("\n>>> Called decorated func <<<", end="\n")
+        out = input_func(*args)
+        print("\n\t^^^")
+        return not out
+    return result
 
-# input("Func will be called...")
+input("Func will be called...")
 
-# @decor # Using the decorator (he get func inself)
-# def result_func(txt = "some txt"):
-#     print(str(txt))
-#     return True
+@decor # Using the decorator (he get func inself)
+def result_func(txt = "some txt"):
+    print(str(txt))
+    return True
 
-# print("Her result is: " + str(result_func("\tsome func")))
+print("Her result is: " + str(result_func("\tsome func")))
 
-# class Commodity:
-#     def __init__(self, price):
-#         self.price = price
+class Commodity:
+    def sell_with_price(self, price):
+        print(f"Commodity selled with price: {price}")
 
-#     def display_price(self):
-#         print(f"Price is: {self.price}")
+class Human:
+    default_name = "Default huma" # атрибут класса (обращаться через имя класса)
+    @staticmethod
+    def say_bae():
+        print("Bye babe!")
 
-# class Negr:
-#     def __init__(self, name, price, age):
-#         self.__name = name
-#         super().__init__(price)
-#         self.__age = age
+    def __init__(self, name):
+        if name:
+            self.__name = name # атрибут объекта (обращаться через экземпляр класса)
+        else: self.__name = Human.default_name
 
-# #>>> Property
-#     @property
-#     def Age(self):
-#         return self.__age
+    #>>> Getter property
+    @property
+    def Name(self):
+        return "Negger name: " + self.__name
+
+    def say_hello(self):
+        print(f"Hello! Im heman end my name is {self.__name}")
+
+class Negr (Human, Commodity):
+    def __init__(self, name, price, age):
+        self.price = price
+        self.__age = age
+        super().__init__(name)
+    def say_hello(self):
+        super().say_hello()
+        print("Im a negra")
+
+#>>> Property
+    @property
+    def Age(self):
+        return self.__age
     
-#     @Age.setter
-#     def Age(self, value):
-#         try:
-#             if int(value) > 0 and int(value) < 110:
-#                 self.__age = value
-#             else: print("Not correct age")
-#         except: print("Not num age")
-#         finally: print("Age setted")
-# #End of property <<<
+    @Age.setter
+    def Age(self, value):
+        try:
+            if int(value) > 0 and int(value) < 110:
+                self.__age = value
+            else: print("Not correct age")
+        except: print("Not num age")
+        finally: print("Age setted")
+#End of property <<<
+    def __del__(self):
+        self.sell_with_price(self.price)
 
-# #>>> Getter property
-#     @property
-#     def Name(self):
-#         return "Negger name: " + self.__name
+    def get_all_data(self):
+        return self.price, self.__age, self.Name 
 
+bobba = Negr("Bobba", 102, 35)
+zeli = Negr("Zelli", 18, 12)
 
-#     def __del__(self):
-#         print(f"Slave {self.__name} selled.")
+print(f"Negrils {bobba.Name} and {zeli.Name} adds to cart. Total price = {bobba.price + zeli.price}")
 
-# bobba = Negr("Bobba", 102, 35)
-# zeli = Negr("Zelli", 18, 12)
+bobba.Age = input("Set bobbas age: ")
+print(f"{bobba.Name} age is {bobba.Age}")
 
-# print(f"Negrils {bobba.Name} and {zeli.Name} adds to cart. Total price = {bobba.price + zeli.price}")
+bobba.say_hello()
 
-# bobba.Age = input("Set bobbas age: ")
-# print(f"{bobba.Name} age is {bobba.Age}")
+if isinstance(zeli, Commodity):
+    zeli.sell_with_price(zeli.price)
+    Human.say_bae()
 
-
+print(zeli.get_all_data())
 
 # # prTxt = input("Nigga: ") # bt not a niga
 # # print(prTxt)
